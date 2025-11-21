a728e58a2751b20b66322bae7562941d428f3c46
Adrien Russo <adrien@appro-auto.com>
2025-11-21 16:43:36 +0100

    feat: Added TamponController and TamponService with tampon tracking and management features

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Service\TamponService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
+    public function __construct(
+        private TamponService $tamponService
+    ) {
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $hasActiveTampon = null;
+
+        if ($this->getUser()) {
+            $hasActiveTampon = $this->tamponService->hasActiveTampon($this->getUser());
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'hasActiveTampon' => $hasActiveTampon,
         ]);
     }
 }
